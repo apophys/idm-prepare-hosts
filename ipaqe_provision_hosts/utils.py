f624ca7894037361c441d4c5834e4a1fe66c991d
Milan Kubík <mkubik@redhat.com>
2017-03-16 17:29:08 +0100

    Add simple parser for os-release to get OS version

--- ipaqe_provision_hosts/utils.py
+++ ipaqe_provision_hosts/utils.py
@@ -43,3 +43,26 @@ def load_topology(path):
     """Load the topology file"""
     log.info("Loading topology file %s", path)
     return load_yaml(path)
+
+
+def get_os_version():
+    """Get the OS version from /etc/os-release
+
+    The function returns pair (ID, VERSION_ID).
+    If the OS does not have VERSION_ID, it will be None
+    """
+
+    try:
+        log.debug('Reading os-release')
+        with open('/ect/os-release') as f:
+            os_release = dict([
+                line.strip().split('=') for line in f.readlines() if line
+            ])
+
+        return (os_release['ID'], os_release.get('VERSION_ID'))
+    except IOError:
+        log.error('The file /etc/os-release was not found.')
+        raise IPAQEProvisionerError
+    except KeyError:
+        log.error("The key ID of os-release was not found.")
+        raise IPAQEProvisionerError
