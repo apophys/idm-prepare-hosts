95b8b73251d08e3475705586f0e453f98add2a7a
Milan Kubík <mkubik@redhat.com>
2017-03-20 18:47:10 +0100

    Add paths module

---
+++ ipaqe_provision_hosts/paths.py
@@ -0,0 +1,5 @@
+# Author: Milan Kubik
+
+DEFAULT_STATIC_INVENTORY = '/etc/ipaqe-provision-hosts/static-domains.yaml'
+SYSTEM_CONFIG_PATH = '/etc/ipaqe-provision-hosts/config.yaml'
+OS_RELEASE = '/etc/os-release'
