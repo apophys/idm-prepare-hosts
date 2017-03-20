2668c6429511df521d16af299f00991a29ff4471
Milan Kubík <mkubik@redhat.com>
2017-03-20 18:50:20 +0100

    Implement the runner module
    The runned module implements the two functions of the tool.
    
    After reading the yaml files with configuration and topology template
    it instantiates the topology manager and uses it to process
    the topology as defined by the template.

--- ipaqe_provision_hosts/runner.py
+++ ipaqe_provision_hosts/runner.py
@@ -1,9 +1,18 @@
 # Author: Milan Kubik
 
 import logging
+import yaml
 
+from ipaqe_provision_hosts.utils import load_config, load_topology
+from ipaqe_provision_hosts.errors import IPAQEProvisionerError
+from ipaqe_provision_hosts.backend.loader import load_backends
+from ipaqe_provision_hosts.static_inventory import StaticInventory
+from ipaqe_provision_hosts.topology_manager import TopologyInventory
+
+from ipaqe_provision_hosts import paths
 
 log = logging.getLogger(__name__)
+
 
 def create(topology, config, output):
     """Initialize backend and prepare configuration
@@ -12,7 +21,57 @@ def create(topology, config, output):
     and uses the configured backend.
     """
 
-    pass
+    log.debug("Running the create command")
+
+    try:
+        # Load configuration files
+        config = load_config(config)
+        topology_template = load_topology(topology)
+
+        # Initialize backend map
+        backends = load_backends()
+
+        # Prepare relevant parts of configuration
+        # Instantiate backend according to configuration
+        try:
+            b_config = config['backend']
+            backend = backends[b_config['name']](b_config['config'])
+        except KeyError as e:
+            log.error(
+                "Problem accessing backend configuration "
+                "or instantiating a backend")
+            raise IPAQEProvisionerError
+
+        log.debug("Retrieving static inventory file path")
+        static_resources_path = (
+            config['core'].get('static_resources',
+                               paths.DEFAULT_STATIC_INVENTORY)
+        )
+        static_resources_os_version = (
+            config['core'].get('os_version')
+        )
+        static_inventory = StaticInventory(
+            static_resources_path, os_version=static_resources_os_version)
+
+        # Instantiate the TopologyManager
+        inventory = TopologyInventory(backend, static_inventory)
+
+        dns_forwarder, provisioned_domains = (
+            inventory.provision_topology(topology_template))
+        config['domain_settings']['dns_forwarder'] = dns_forwarder
+
+        multihost_config = {}
+
+        multihost_config['domains'] = provisioned_domains
+        multihost_config.update(config['domain_settings'])
+
+        with open(output, mode='w') as f:
+            log.debug("Writing the configuration file to %s", output)
+            yaml.safe_dump(multihost_config, f, default_flow_style=False)
+
+    except IPAQEProvisionerError as e:
+        log.debug("Aborting create call: %s", e.message)
+        raise
 
 
 def delete(config):
@@ -23,4 +82,27 @@ def delete(config):
 
     The backend itself is responsible for managing caches.
     """
-    pass
+    log.debug("Running the delete command")
+
+    try:
+        # Load configuration files
+        config = load_config(config)
+        # Initialize backend map
+        backends = load_backends()
+
+        # Prepare relevant parts of configuration
+        # Instantiate backend according to configuration
+        try:
+            b_config = config['backend']
+            backend = backends[b_config['name']](b_config['config'])
+        except KeyError as e:
+            log.error(
+                "Problem accessing backend configuration "
+                "or instantiating a backend")
+            raise IPAQEProvisionerError
+
+        backend.delete_resources()
+
+    except IPAQEProvisionerError as e:
+        log.debug("Aborting delete call: %s", e.message)
+        raise
